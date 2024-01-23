b5682c809ea5103f33943385ed0a589456fd4151
Daniel Ochuba <danielochuba78@gmail.com>
2024-01-23 08:26:36 +0100

    modify migration to use jti

--- db/migrate/20240106203041_add_jit_to_users.rb
+++ db/migrate/20240106203041_add_jit_to_users.rb
@@ -1,6 +1,6 @@
 class AddJitToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :jit, :string, null: false
-    add_index :users, :jit, unique: true
+    add_column :users, :jti, :string, null: false
+    add_index :users, :jti, unique: true
   end
 end
